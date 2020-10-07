31099d2b6ac11f7b44476b34deb37f5f370bf5a3
Mouni Nischal <d.chiranjeevi007@gmail.com>
2020-10-08 00:30:05 +0530

    Updates

---
+++ precache-manifest.ac791ca8e6e61348db03cdcdc6b2c95f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2823f5aa14d067a7e14bab68e2ce5526",
+    "url": "/reacttodoapp/index.html"
+  },
+  {
+    "revision": "25f7f1dc7bd4d49b89fa",
+    "url": "/reacttodoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e015ecd0fa5a3c8c3a86",
+    "url": "/reacttodoapp/static/js/2.a8cfda1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodoapp/static/js/2.a8cfda1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f7f1dc7bd4d49b89fa",
+    "url": "/reacttodoapp/static/js/main.fe3b20c3.chunk.js"
+  },
+  {
+    "revision": "783ca65bc9a6c7494248",
+    "url": "/reacttodoapp/static/js/runtime-main.c3db828e.js"
+  }
+]);
